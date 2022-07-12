7d0b45300722c5c9f240e2bb583ce56304954a88
ampersand-paris <ampersand.s.paris@gmail.com>
2022-07-12 08:45:18 -0400

    typo

--- src/Components/DesignServices/Services.js
+++ src/Components/DesignServices/Services.js
@@ -159,7 +159,7 @@ const Services = () => {
     return (
         <div className="standard-spacing  bg-light-pink">
             <h1 className="page-title bubblegum">Services</h1>
-            <h2 className="intro">FAILSPACE Design Services works with a wide range of clients from individual artists and artis institutions to small businesses and large companies looking to develop their digital prescence.</h2>
+            <h2 className="intro">FAILSPACE Design Services works with a wide range of clients from individual artists and arts institutions to small businesses and large companies looking to develop their digital prescence.</h2>
             <div className="services-page  mobile-column">
                 <div className="service-list">
                     <div onClick={toggleWebDev} className="service" id="ivy" style={{backgroundColor: ivy}}>
