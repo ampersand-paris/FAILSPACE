06093cf2fd1103aab5d446844d6046b244e0f2ec
ampersand-paris <ampersand.s.paris@gmail.com>
2022-09-19 15:06:54 -0400

    portfolio mobile

--- src/Components/DesignServices/Portfolio.js
+++ src/Components/DesignServices/Portfolio.js
@@ -8,11 +8,11 @@ import { Link } from "react-router-dom"
 const Portfolio = () => {
 
     return (
-        <div className="standard-spacing bg-light-pink">
+        <div className="full-height standard-spacing bg-light-pink">
             <h1 className="page-title bubblegum">Portfolio</h1>
             <div className="extra-margin">
                 <div className="portfiolio-center">
-                    <div className="flex jc-space-btwn side-padding-2vW bg-off-black light-pink">
+                    <div className="flex jc-space-btwn mobile-column side-padding-2vW bg-off-black light-pink">
                         <h3 className="third no-space">Client</h3>
                         <h3 className="third no-space">Services</h3>
                         <h3 className="third no-space">Technologies</h3>
@@ -20,44 +20,44 @@ const Portfolio = () => {
                     <Link 
                             to="/design-services/portfolio/performance-primers"
                             className="no-hyperlink-style"  >
-                        <div className="cursor flex side-padding-2vW top-padding-2vH border-bttm-off-black">
-                            <h4 className="third no-space">Performance Primers</h4>
+                        <div className="cursor flex mobile-column jc-baseline side-padding-2vW top-padding-2vH border-bttm-off-black">
+                            <h4 className="third no-space bigger-h4">Performance Primers</h4>
                             <h4 className="third no-space">Web Development, Brand Identity</h4>
-                            <h4 className="third no-space">Cargo, Custom HTML & CSS</h4>
+                            <h4 className="third no-space">Cargo, HTML & CSS</h4>
                         </div>
                     </Link> 
                     <Link 
                             to="/design-services/portfolio/experiments-in-freedom"
                             className="no-hyperlink-style"  >
-                        <div className="cursor flex side-padding-2vW top-padding-2vH border-bttm-off-black">
-                            <h4 className="third no-space">Experiments in Freedom</h4>
+                        <div className="cursor flex mobile-column jc-baseline side-padding-2vW top-padding-2vH border-bttm-off-black">
+                            <h4 className="third no-space bigger-h4">Experiments in Freedom</h4>
                             <h4 className="third no-space">Web Development, Brand Identity</h4>
-                            <h4 className="third no-space">Cargo, Custom HTML, CSS & JavaScript</h4>
+                            <h4 className="third no-space">Cargo, HTML, CSS & JavaScript</h4>
                         </div>
                     </Link> 
                     <Link 
                             to="/design-services/portfolio/cpr"
                             className="no-hyperlink-style"  >
-                        <div className="cursor flex side-padding-2vW top-padding-2vH border-bttm-off-black">
-                            <h4 className="third no-space">Center for Performance Research</h4>
+                        <div className="cursor flex mobile-column jc-baseline side-padding-2vW top-padding-2vH border-bttm-off-black">
+                            <h4 className="third no-space bigger-h4">Center for Performance Research</h4>
                             <h4 className="third no-space">Web Development, Brand Identity</h4>
-                            <h4 className="third no-space">Squarespace, Custom HTML & CSS</h4>
+                            <h4 className="third no-space">Squarespace, HTML & CSS</h4>
                         </div>
                     </Link> 
                     <Link 
                             to="/design-services/portfolio/ethan-philbrick"
                             className="no-hyperlink-style"  >
-                        <div className="cursor flex side-padding-2vW top-padding-2vH border-bttm-off-black">
-                            <h4 className="third no-space">Ethan Philbrick</h4>
+                        <div className="cursor flex mobile-column jc-baseline side-padding-2vW top-padding-2vH border-bttm-off-black">
+                            <h4 className="third no-space bigger-h4">Ethan Philbrick</h4>
                             <h4 className="third no-space">Web Development, Brand Identity</h4>
-                            <h4 className="third no-space">Cargo CMS, Custom HTML & CSS</h4>
+                            <h4 className="third no-space">Cargo CMS, HTML & CSS</h4>
                         </div>
                     </Link> 
                     <Link 
                             to="/design-services/portfolio/rebecca-fitton"
                             className="no-hyperlink-style"  >
-                        <div className="cursor flex side-padding-2vW top-padding-2vH border-bttm-off-black">
-                            <h4 className="third no-space">Rebecca Fitton</h4>
+                        <div className="cursor flex mobile-column jc-baseline side-padding-2vW top-padding-2vH border-bttm-off-black">
+                            <h4 className="third no-space bigger-h4">Rebecca Fitton</h4>
                             <h4 className="third no-space">Print Publication</h4>
                             <h4 className="third no-space"></h4>
                         </div>
